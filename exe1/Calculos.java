0e2e3808a2db4f48ce03c53a484e815e74e6cca3
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-04-07 21:09:36 -0300

    Create Calculos.java

---
+++ exe1/Calculos.java
@@ -0,0 +1,18 @@
+public class Calculos {
+    public static int soma(int a ,int b){
+        return a + b;
+    }
+
+    public static int divisao(int a ,int b){
+        return a / b;
+    }
+
+    public static int mult(int a ,int b){
+        return a * b;
+    }
+
+    public static int sub(int a ,int b){
+        return a - b;
+    }
+}
+
