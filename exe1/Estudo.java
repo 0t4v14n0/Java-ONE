81451425558ded1e25295becb4009235198a5880
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-04-07 21:09:54 -0300

    Create Estudo.java

---
+++ exe1/Estudo.java
@@ -0,0 +1,8 @@
+public class Estudo {
+    public static void Mesagens() {
+        System.out.println("Anotações de estudos");
+        System.out.println("1- Estudar Java ");
+        System.out.println("2- Estudar POO ");
+        System.out.println("3 -Relembrar conceitos");
+    }
+}
