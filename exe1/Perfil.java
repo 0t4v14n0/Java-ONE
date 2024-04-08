5563abd811330018d577f49a3bc08381a8a452e7
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-04-07 21:08:56 -0300

    Create Perfil.java

---
+++ exe1/Perfil.java
@@ -0,0 +1,60 @@
+/* Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil,
+com o método main, que ao executar deverá imprimir um cumprimento personalizado
+no console. Por exemplo, "Olá, [Seu Nome]!".
+Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada.
+Por exemplo, "Tudo bem?".
+Crie uma classe chamada Estudos para imprimir anotações de estudos no console.
+Crie uma classe Soma em Java para imprimir no console o resultado da operação
+10 + 5. Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando
+o valor "15" ao invés da expressão "10+5".
+Crie uma classe Subtração para imprimir o resultado de 10 - 5 no console.*/
+
+import java.util.Scanner;
+
+public class Perfil {
+    public static <integer> void main(String[] args) {
+        //INICIA O SCAN
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual e seu nome ?");
+        String nome = scanner.nextLine();// Ler a linha d ebaixo
+
+        System.out.println("Olá, " + nome + "!");
+        System.out.println("Tudo bem?, vamos calcualar...");
+
+        //tem que usar o Integer.parseInt pq o scaner sempre vai voltar uma string...
+
+        System.out.println("Primeiro valor ?");
+        int a = Integer.parseInt( scanner.nextLine());
+
+        System.out.println("Segundo valor ?");
+        int b = Integer.parseInt( scanner.nextLine());
+
+        System.out.print("1 - soma " +
+                "2- Subtracao " +
+                "3- Multiplicacao " +
+                "4- Divisao ");
+
+        int ope = Integer.parseInt( scanner.nextLine());
+
+        switch(ope){
+            case 1:
+                System.out.println("O resultado e: " + Calculos.soma(a,b) + "!");
+                break;
+            case 2:
+                System.out.println("O resultado e: " + Calculos.sub(a,b) + "!");
+                break;
+            case 3:
+                System.out.println("O resultado e: " + Calculos.mult(a,b) + "!");
+                break;
+            case 4:
+                System.out.println("O resultado e: " + Calculos.divisao(a,b) + "!");
+                break;
+        }
+
+        //TERMINA O SCANN
+        scanner.close();
+
+        Estudo.Mesagens();
+    }
+}
