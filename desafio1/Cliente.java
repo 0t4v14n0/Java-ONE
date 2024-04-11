215fc63e0d144d77aa9428f837eb35bcfaf87dcb
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-04-11 04:26:45 -0300

    Create Cliente.java

---
+++ desafio1/Cliente.java
@@ -0,0 +1,55 @@
+package bancodesafio;
+
+public class Cliente {
+	
+	private  int   id;
+	private String nome;
+	private double saldo;
+	private String tipoconta;
+	
+	public Cliente(int id ,String nome, double saldo, String tipoconta) {
+		this.id = id;
+        this.nome = nome;
+        this.saldo = saldo;
+        this.tipoconta = tipoconta;
+    }
+	
+	//gets e sets...
+	
+	public int getId () {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public String getTipoconta() {
+		return tipoconta;
+	}
+	
+	public void setTipoconta(String tipoconta) {
+		this.tipoconta = tipoconta;
+	}
+	
+	public String toString() {
+        return " - id: " + id + " - nome: " + nome + " - Saldo: R$"+ saldo +" - Tipo de Conta: "+tipoconta;
+    }
+
+}
