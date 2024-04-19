ef9f64102e9342dc8fad980d9b92f7f66be002aa
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-04-19 02:54:02 -0300

    Update Main.java

--- exe10/at4/Main.java
+++ exe10/at4/Main.java
@@ -1,3 +1,7 @@
+/*
+4 - Crie uma classe Produto com propriedades como nome e preço. Em seguida, crie uma lista de produtos e utilize um loop para calcular e imprimir o preço médio dos produtos.
+*/
+
 package at4;
 
 import java.util.ArrayList;
