b0e6f69edb7520f89d9373d541ad1467f9c74ec0
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-05-07 01:24:45 -0300

    Update Main.java

--- desafio4/src/com/cep/Main.java
+++ desafio4/src/com/cep/Main.java
@@ -1,24 +1,46 @@
 package com.cep;
 
 import java.io.IOException;
+import java.util.Scanner;
 import com.cep.util.ApiCEP;
 import com.cep.util.Cep;
+import com.cep.util.GerarArquivo;
 
 public class Main {
     
     public static void main(String[] args) {
-        
-        String cep = "55150005";
-        
-        ApiCEP apiCEP = new ApiCEP();
-        
-        try {
-            Cep novoCep = apiCEP.consultarEnderecoPeloCEP(cep);
-            System.out.println(novoCep);
-        } catch (IOException | InterruptedException e) {
-            System.err.println("Não foi possível obter o endereço a partir desse CEP.");
-            e.printStackTrace();
-        }
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	do {
+    		
+        	System.out.println("Buscar CEP ou sair");
+    		String cep = scanner.nextLine();
+    		
+    		if (cep.equals("sair") || cep.equals("Sair") || cep.equals("exit") || cep.equals("Exit")) {
+    			
+    			break;
+                
+    		}
+    		else {
+    			
+                ApiCEP apiCEP = new ApiCEP();
+                
+                try {
+                	
+                    Cep novoCep = apiCEP.consultarEnderecoPeloCEP(cep);
+                    System.out.println(novoCep);
+                    GerarArquivo gerador = new GerarArquivo();
+                    gerador.salvaJson(novoCep);
+                    
+                } catch (IOException | InterruptedException e) {
+                    System.err.println("Não foi possível obter o endereço a partir desse CEP.");
+                    e.printStackTrace();
+                }
+                
+    		}
+    		
+    	}while(true);
     
     }
 }
