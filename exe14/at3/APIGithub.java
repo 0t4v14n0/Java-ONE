7c6193913d2294667e01277573252e7a1cfd11f1
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-05-13 10:30:48 -0300

    Update APIGithub.java

--- exe14/at3/APIGithub.java
+++ exe14/at3/APIGithub.java
@@ -1,6 +1,7 @@
 package at3;
 
 import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,11 +9,11 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class APIGithub {
-	
-	private static final String BASE_URL = "https://api.github.com/users/";
-    
-    public Usuario busca(String usuario) throws IOException, InterruptedException { 
-        
+
+    private static final String BASE_URL = "https://api.github.com/users/";
+
+    public Usuario busca(String usuario) throws IOException, InterruptedException, ErroConsultaGitHubException {
+
         String endereco = BASE_URL + usuario;
 
         HttpClient client = HttpClient.newHttpClient();
@@ -20,10 +21,14 @@ public class APIGithub {
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());        
-        
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 404) {
+            throw new ErroConsultaGitHubException("Usuário não encontrado no GitHub: " + usuario);
+        }
+
         return new Gson().fromJson(response.body(), Usuario.class);
-        
+
     }
 
 }
