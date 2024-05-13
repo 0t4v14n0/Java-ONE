7ee07a9eb3371d313ae3e9d7ef7fb7dbb2ed6926
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-05-13 10:30:33 -0300

    Update Main.java

--- exe14/at3/Main.java
+++ exe14/at3/Main.java
@@ -5,26 +5,27 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner leitura = new Scanner(System.in);
-		
-		try {
-			
-			System.out.println("Digite nome do usuario: ");
-	        String nome = leitura.nextLine();
-	        
-	        APIGithub consu = new APIGithub();
-	        
-			Usuario user = consu.busca(nome);
-			
-			System.out.println(user);
-			
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
 
-	}
+        Scanner leitura = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Digite o nome do usuário do GitHub: ");
+            String nome = leitura.nextLine();
+
+            APIGithub consu = new APIGithub();
+
+            Usuario user = consu.busca(nome);
+
+            System.out.println(user);
+
+        } catch (ErroConsultaGitHubException e) {
+            System.out.println("Erro ao consultar o usuário do GitHub: " + e.getMessage());
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro de entrada/saída ao consultar o usuário do GitHub: " + e.getMessage());
+        }
+
+    }
 
 }
